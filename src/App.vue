7783d0217e6dc15312e6f2d9a165a1e4d6c583f6
Ryan Brockley <rybrockers@yahoo.com>
2019-08-19 19:53:40 +0100

    project setup

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div class="container">
+    <Todos />
+    </div>
+  </div>
+</template>
+
+<script>
+import Todos from "@/components/Todos";
+export default {
+  name: "App",
+  components: {
+    Todos
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container {
+  max-width: 1100px;
+  margin: 0;
+  overflow: auto;
+  padding: 0 2rem;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="todos">
+    <h1>Todos</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todos',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
